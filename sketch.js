0534d7e1bc7184acb6c3929a384e08c3c11febdb
minseg <artssam7602@naver.com>
2024-11-05 16:10:28 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,42 +2,35 @@ let ball;
 
 function setup() {
   createCanvas(400, 200);
-
   ball = new Particle();
-  wind = createVector(0, 0);
+  wind = createVector(0, 0); // 바람 초기화
 }
 
 function draw() {
-    background(220);
+  background(220);
+
+  // 중력 벡터 설정 (아래 방향)
+ // let gravity = createVector(0, 0.3);
+  //ball.addForce(gravity);
+
+  //if (mouseIsPressed) {
+ // let wind = createVector(0.1, 0);
+ // ball.addForce(wind);
+  //}
+
   
-  // let gravity = createVector(0, 0.3);
-  // ball.addForce(gravity);
-
-  // if(mouseIsPressed){
-  // let wind = createVector(0.1, 0);
-  // ball.addForce(wind);
-  // }
-
-  //let took = createVector();
+  ball.pos
   
 
-    ball.pos
-
-    ball.update();
-    ball.show();
+  // 파티클 업데이트 및 표시
+  ball.update();
+  ball.show();
 }
- 
-  
 
-  function mouseCliked(){
-
-  //let took = createVector();
-  
-  let mPos = createVector(mouseX, mouseY);
-  let took = p5.Vector.sub(ball.pos, mPos);
-  took.mult(0.1);
-  ball.addForce(took);
-  }
-  
-
-
+function mouseClicked() {
+//let took = createVector();
+let mPos = createVector(mouseX, mouseY);
+let took = p5.Vector.sub(ball.pos, mPos);
+took.mult(0.1);
+ball.addForce(took);
+}
